184518c4acb07f0fb67216445435d5d136c2daf5
Michael Ezzo <ezzomich@msu.edu>
2018-09-17 22:15:29 -0400

    try diff

--- custom_components/sensor/life360.py
+++ custom_components/sensor/life360.py
@@ -228,8 +228,8 @@ class Life360SensorData(object):
             _LOGGER.error("Output: %s", output)
             output = output.strip().decode('utf-8')
 
-        except subprocess.CalledProcessError:
-            _LOGGER.error("Command failed: %s", command)
+        except subprocess.CalledProcessError as exc:
+            _LOGGER.error("Status : FAIL", exc.returncode, exc.output)
             self.value = CONST_STATE_ERROR
             output = None
         except subprocess.TimeoutExpired:
