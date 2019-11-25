7934c4731e3982f54e88733547c8e1a6a2099664
Michael Ezzo <ezzomich@msu.edu>
2019-11-25 18:03:24 -0500

    use pypi library instead.

--- custom_components/surveillance_station/__init__.py
+++ custom_components/surveillance_station/__init__.py
@@ -1,49 +1,68 @@
 """Synology Surveillance Station."""
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import logging
+from requests.exceptions import RequestException
 import voluptuous as vol
-from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_URL)
+from homeassistant.const import (
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_URL,
+    CONF_VERIFY_SSL,
+    CONF_TIMEOUT
+)
 import homeassistant.helpers.config_validation as cv
-from requests import get
 
 _LOGGER = logging.getLogger(__name__)
 
+DEFAULT_TIMEOUT = 5
 DOMAIN = 'surveillance_station'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.All(vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_URL): cv.url
+        vol.Required(CONF_URL): cv.url,
+        vol.Optional(CONF_VERIFY_SSL, default=True): cv.boolean,
+        vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int
      })),
 }, extra=vol.ALLOW_EXTRA)
 
 def setup(hass, config):
     """Set up the Synology Surveillance Station component."""
 
+    # Get values from config
     conf = config[DOMAIN]
+    verify_ssl = conf.get(CONF_VERIFY_SSL)
+    timeout = conf.get(CONF_TIMEOUT)
 
-    username = conf.get(CONF_USERNAME)
-    password = conf.get(CONF_PASSWORD)
-    url = conf.get(CONF_URL)
+    try:
+        from synology.surveillance_station import SurveillanceStation
+
+        surveillance = SurveillanceStation(
+            conf.get(CONF_URL),
+            conf.get(CONF_USERNAME),
+            conf.get(CONF_PASSWORD),
+            verify_ssl=verify_ssl,
+            timeout=timeout,
+        )
+    except (RequestException, ValueError):
+        _LOGGER.exception("Error when initializing SurveillanceStation")
+        return False
+
+    hass.states.set('surveillance_station.home_mode', "home" if surveillance.get_home_mode_status() == "true" else "away")
 
     def handle_set_home_mode(call):
         """Handle the service call."""
 
-        # Create New SynoService
-        synology = SynoService(url)
+        mode = call.data.get('home_mode', 'away').lower()
 
-        # If we successfully login
-        if synology.login(username, password):
-            mode = call.data.get('home_mode', 'away').lower()
-
-            # Set the Home Mode
-            if synology.set_home(True if mode == 'home' else False):
-                _LOGGER.info('Successfully set mode to: %s', mode)
-            else:
-                _LOGGER.error('Failed set mode to: %s', mode)
+        # Set the Home Mode
+        if surveillance.set_home_mode(True if mode == 'home' else False):
+            _LOGGER.info('Successfully set mode to: %s', mode)
+        else:
+            _LOGGER.error('Failed set mode to: %s', mode)
 
         hass.states.set('surveillance_station.home_mode', mode)
 
@@ -51,106 +70,3 @@ def setup(hass, config):
 
     # Return boolean to indicate that initialization was successful.
     return True
-
-class SynoService():
-    def __init__(self, url):
-        self.url = url + '/webapi/'
-        self.sid = None
-        self.synotoken = None
-
-    def login(self, username, password):
-        """Login to synology API"""
-
-        response = get(
-            self.url + 'auth.cgi',
-            params = dict(
-                api = 'SYNO.API.Auth',
-                method = 'Login',
-                version = 6,
-                account = username,
-                passwd = password,
-                session = 'SurveillanceStation',
-                format = 'sid',
-                enable_syno_token = 'yes'
-            )
-        )
-
-        # Get JSON data
-        data = response.json()
-
-        if response.status_code == 200 and data["success"] is True:
-            self.sid = data["data"]["sid"]
-            self.synotoken = data["data"]["synotoken"]
-
-            _LOGGER.debug('Successfully logged into Synology at: %s', self.url)
-            return True
-        else:
-            _LOGGER.error('Failed to login to Synology at: %s', self.url)
-            return False
-
-    def get_home(self):
-        """ Get home mode status of surveillance station"""
-
-        # Fail if not logged in
-        if self.sid is None:
-            return None
-
-        response = get(
-            self.url + 'entry.cgi',
-            params = dict(
-                api = 'SYNO.SurveillanceStation.HomeMode',
-                version = 1,
-                method = 'GetInfo',
-                _sid = self.sid,
-                SynoToken = self.synotoken
-            )
-        )
-
-        data = response.json()
-
-        if response.status_code == 200 and data["success"] is True:
-            return True if data["data"]["on"] else False
-        else:
-            return None
-
-    def set_home(self, is_home):
-        """ Set home mode of surveillance station"""
-
-        # Fail if not logged in
-        if self.sid is None:
-            return False
-
-        response = get(
-            self.url + 'entry.cgi',
-            params = dict(
-                api = 'SYNO.SurveillanceStation.HomeMode',
-                version = 1,
-                method = 'Switch',
-                on = 'true' if is_home else 'false',
-                _sid = self.sid,
-                SynoToken = self.synotoken
-            )
-        )
-
-        return (response.status_code == 200 and response.json()["success"] is True)
-
-    def __del__(self):
-        """Logout automatically on descruction if we're still logged in"""
-
-        if self.sid is not None:
-            response = get(
-                self.url + 'auth.cgi',
-                params = dict(
-                    api = 'SYNO.API.Auth',
-                    method = 'Logout',
-                    version = 1,
-                    session = 'SurveillanceStation',
-                    _sid = self.sid,
-                    SynoToken = self.synotoken
-                )
-            )
-
-            if response.status_code == 200 and response.json()["success"] is True:
-                _LOGGER.debug('Successfully logged out of Synology.')
-            else:
-                _LOGGER.error('Failed to logout of Synology: %s', self.url)
