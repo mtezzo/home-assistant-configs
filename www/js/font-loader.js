c3d396567417a1a39943dbeb6fb695528ec6526c
Michael Ezzo <ezzomich@msu.edu>
2023-03-11 14:04:14 -0500

    create custom font loader.

---
+++ www/js/font-loader.js
@@ -0,0 +1,12 @@
+WebFontConfig = {
+    google: {
+        families: ['Antonio:400,700', 'Open Sans']
+    }
+};
+
+(function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
